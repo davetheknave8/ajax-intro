ef88591bd9d0065d35feb59b6e4eb1769366d54c
David Reeves <davidsreeves1@gmail.com>
2019-06-26 10:58:46 -0500

    added notes to ajax

--- server/Public/client.js
+++ server/Public/client.js
@@ -3,10 +3,14 @@ console.log('hello');
 $(document).ready( onReady );
 
 function onReady() {
+    //ajas is asynchronous $.ajax return a Promise
+    //that Promise says that when the server responds
+    //we do the thing in the `then`
     $.ajax({
         method: 'GET',
         url: '/movies'
     }).then( function (response){
         console.log(`Got some movies!!!`, response);
     })
+    console.log('down here');
 }
