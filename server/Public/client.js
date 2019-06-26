a8219969a9e760684a11e93215d4a979dccad633
David Reeves <davidsreeves1@gmail.com>
2019-06-26 13:37:22 -0500

    finished intro

--- server/Public/client.js
+++ server/Public/client.js
@@ -11,6 +11,19 @@ function onReady() {
         url: '/movies'
     }).then( function (response){
         console.log(`Got some movies!!!`, response);
+        renderMovies(response);
+    }).catch( function(error){
+        console.log(`Something bad happened... `);
+        alert('Something bad happened. Try again later.');
     })
+    //we don't wait for the server to respond before moving on
+    //to run this next line of code, we just do the requesting
     console.log('down here');
-}
+};
+
+function renderMovies(movieList){
+    $('#movies').empty();
+    for(let currentMovie of movieList){
+        $('#movies').append(`<tr><td>${currentMovie.name}</td><td>${currentMovie.movie}</td></tr>`)
+    };
+};
