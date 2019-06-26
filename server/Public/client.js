2f3640baf82cbf075d96f3d84a3c280695742d71
David Reeves <davidsreeves1@gmail.com>
2019-06-26 15:12:14 -0500

    working on server

--- server/Public/client.js
+++ server/Public/client.js
@@ -6,6 +6,7 @@ function onReady() {
     //ajas is asynchronous $.ajax return a Promise
     //that Promise says that when the server responds
     //we do the thing in the `then`
+    getAllMovies();
     $.ajax({
         method: 'GET',
         url: '/movies'
@@ -19,6 +20,8 @@ function onReady() {
     //we don't wait for the server to respond before moving on
     //to run this next line of code, we just do the requesting
     console.log('down here');
+
+   $('#add-movie').on('click', handleAddMovie);
 };
 
 function renderMovies(movieList){
@@ -27,3 +30,45 @@ function renderMovies(movieList){
         $('#movies').append(`<tr><td>${currentMovie.name}</td><td>${currentMovie.movie}</td></tr>`)
     };
 };
+
+function handleAddMovie(){
+    //Get info from input fields
+    let name = $('#in-name').val();
+    let movie = $('#in-movie').val();
+
+    $.ajax({
+        method: 'POST',
+        url: '/movies',
+        data: {
+            name: name,
+            movie: movie
+        }
+    })
+    .then( function (response) {
+        //POST (add movie) was good
+        //Clear out input fields on form
+        $('#in-name').val('');
+        $('#in-movie').val('');
+        
+        //GET all my movies again, so the new one shows on the page
+        getAllMovies();
+    })
+    .catch( function(error) {
+        console.log(`Something bad happened... `);
+        alert('Something bad happened. Try again later.');
+    })
+
+}
+
+function getAllMovies(){
+    $.ajax({
+        method: 'GET',
+        url: '/movies'
+    }).then(function (response) {
+        console.log(`Got some movies!!!`, response);
+        renderMovies(response);
+    }).catch(function (error) {
+        console.log(`Something bad happened... `);
+        alert('Something bad happened. Try again later.');
+    })
+}
--- server/server.js
+++ server/server.js
@@ -6,13 +6,22 @@ const app = express();
 //Setup static files
 app.use( express.static( 'server/public'));
 
+//When we do a POST and want to get data from a request we need help
+//We need body-parser
+let bodyParser = require('body-parser');
+
 //Setup route to return movies
 const movieData = require('./modules/movie.module.js')
 app.get( '/movies', (req, res) => {
     res.send(movieData);
 })
 
-
+//Post new movie to server
+app.post( '/movies', (req, res) => {
+    let newMovie = ???
+    //Add it onto the array of movies
+    movieData.push(newMovie);
+})
 //Start the server listening - do this last, after setting up routes, and all the things
 const PORT = 5000;
 app.listen( PORT, () => {
